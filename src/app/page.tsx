8ad5e31b54d1e7822bcbd9bd0a68a83cd99e62d7
Himanshu Sinha <ashusinha1904@gmail.com>
2025-12-24 22:05:37 +0530

    Folder structure updated

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { trpc } from "@/trpc/client";
-
-export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "World" });
-  return <Button className="m-4">{data?.greeting ?? "Loading..."}</Button>;
-}
