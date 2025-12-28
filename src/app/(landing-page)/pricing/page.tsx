b67b5ec44ac415811b3421c529e2825819eade35
Himanshu Sinha <ashusinha1904@gmail.com>
2025-12-28 11:37:06 +0530

    Pricing page added

---
+++ src/app/(landing-page)/pricing/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { CheckIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { cn } from "@/lib/utils";
+
+import { pricingPlans } from "@/constants/pricing-plans";
+
+const PricingPage = () => {
+  const router = useRouter();
+  return (
+    <MaxWidthWrapper>
+      <section className="flex flex-col gap-y-8 mt-6">
+        <div className="flex flex-col items-center gap-y-2">
+          <h2 className="text-3xl font-medium text-primary">Pricing</h2>
+          <p className="text-gray-700 text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+            deserunt.
+          </p>
+        </div>
+
+        <div className="flex justify-center lg:justify-between flex-wrap items-center gap-5 lg:gap-0">
+          {pricingPlans.map((plan) => {
+            return (
+              <Card
+                key={plan.name}
+                className={cn(
+                  "w-[32%] flex flex-col items-center justify-center min-w-67.5",
+                  plan.highlighted ? "border-2 border-primary" : ""
+                )}
+              >
+                <CardHeader className="w-full">
+                  <CardTitle className="text-xl">{plan.name}</CardTitle>
+                  <p className="text-3xl font-bold">{plan.price}</p>
+                  <CardDescription>{plan.description}</CardDescription>
+                </CardHeader>
+
+                <CardContent className="w-full flex flex-col gap-y-4">
+                  <p className="text-sm text-gray-700">Features:</p>
+
+                  <div className="flex flex-col gap-y-2">
+                    {plan.features
+                      .map((item) => {
+                        return (
+                          <div key={item} className="flex gap-x-2 items-center">
+                            <CheckIcon className="text-primary" size={18} />
+                            <p key={item} className="text-sm">
+                              {item}
+                            </p>
+                          </div>
+                        );
+                      })}
+                  </div>
+
+                  <Button
+                    variant={plan.highlighted ? "default" : "outline"}
+                    size={"lg"}
+                  >
+                    {plan.buttonText}
+                  </Button>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
+    </MaxWidthWrapper>
+  );
+};
+
+export default PricingPage;
---
+++ src/constants/pricing-plans.ts
@@ -0,0 +1,24 @@
+export const pricingPlans = [
+  {
+    name: "Free",
+    features: ["3 links", "Basic Customization", "Limited support"],
+    price: "₹0/month",
+    description: "Free for you to get started",
+    buttonText: "Signup for Free",
+  },
+  {
+    name: "Basic",
+    features: ["Everything in Free", "5 links", "Analytics"],
+    price: "₹50/month",
+    description: "Enough for most needs",
+    highlighted: true,
+    buttonText: "Get Started",
+  },
+  {
+    name: "Pro",
+    features: ["Everything in Pro", "Unlimited links", "24X7 support"],
+    price: "₹100/month",
+    description: "For the Pros!",
+    buttonText: "Get Started",
+  },
+];
